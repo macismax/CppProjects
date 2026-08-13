3df22e8b31fa1526c3ca11e7ae77b313725d7a48
macismax <max.vasquezz04@gmail.com>
2026-08-13 11:19:18 -0500

    files

---
+++ Introduction00/main.cpp
@@ -0,0 +1,11 @@
+//Basic programs from the learncpp.com website//
+
+#include <iostream>
+
+int main()
+{ 
+    std::cout << "random text goes here\n";
+    //learned that \n is a better habit since it dosent flush the output buffer like std::endl does//
+
+    
+}
