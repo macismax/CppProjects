02e778e87d4dbbee996c61307eed9d506f688f4f
macismax <max.vasquezz04@gmail.com>
2026-08-14 13:26:38 -0500

    Final chapter overview

---
+++ Basics01/chapterSum.cpp
@@ -0,0 +1,40 @@
+#include <iostream>              //summary of chapter 1 
+
+int main ()
+{
+
+    std:: cout << "=== Monthly Expenses Calculator ===\n\n";
+
+    int monthlyIncome {};
+    int housingCost {};
+    int foodCost {};
+    int transportationCost {};
+    int entertainmentCost {};
+    int otherExpenses {};
+    
+    std::cout << "Enter your monthly income: ";
+    std::cin >> monthlyIncome;
+
+    std::cout << "Enter your housing cost: ";
+    std::cin >> housingCost;
+
+    std::cout << "Enter your food cost: ";
+    std::cin >> foodCost;
+
+    std::cout << "Enter your transportation cost: ";
+    std::cin >> transportationCost;
+
+    std::cout << "Enter your entertainment cost: ";
+    std::cin >> entertainmentCost;
+
+    std::cout << "Enter your other expenses: ";
+    std::cin >> otherExpenses;
+
+    std:: cout << "===== Monthly Summary =====\n";
+
+    std:: cout << "Income: $" << monthlyIncome << "\n";
+    std:: cout << "Total Expenses: $" << (housingCost + foodCost + transportationCost + entertainmentCost + otherExpenses) << "\n";
+    std:: cout << "Remaining Balance: $" << (monthlyIncome - (housingCost + foodCost + transportationCost + entertainmentCost + otherExpenses)) << "\n";        
+    std:: cout << "percent of income spendt: " << ((monthlyIncome - (housingCost + foodCost + transportationCost + entertainmentCost + otherExpenses)) / monthlyIncome) * 100 << "%\n";
+
+}
